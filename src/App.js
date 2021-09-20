1494b2857cb33ed8fd2a7f2577bfe38a12d1f88f
Anna Ciszak <aniac600@gmail.com>
2021-09-20 23:30:49 +0200

    Add task with button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {button, useState} from 'react';
+
+function addToDo(todos, setTodos){
+  setTodos([...todos, 'third'])
+}
 
 function App() {
+
+  const[todos, setTodos] = useState(['first','second']);
+  const toDoList = todos.map((element) =>
+  <li>{element}</li>);
+
   return (
     <div className="App">
+    <button onClick={() => addToDo(todos, setTodos)}>Add</button>
     <ul>
-    <li></li>
-    <li></li>
-    <li></li>
+    {toDoList}
     </ul>
-
     </div>
   );
 }
